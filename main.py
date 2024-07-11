4f03f3ee9db9a12d4f4b817db676c629d2d4c8d4
Owais Shaikh <owais.shaikh2378@gmail.com>
2024-07-11 13:24:52 +0500

    Image Watermarking App

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from PIL import Image, ImageDraw, ImageFont
-from tkinter import Tk
-from tkinter import filedialog
-from tkinter import Label
+import os.path
 from tkinter import Button
 from tkinter import Entry
+from tkinter import Label
+from tkinter import Tk
+from tkinter import filedialog
+
+from PIL import Image, ImageDraw, ImageFont
 
 
 def add_watermark():
     user_input = text_entry.get()
-    root.destroy()
-    file = filedialog.askopenfilename(initialdir="D:/", title="UPLOAD AN IMAGE")
+    file = filedialog.askopenfilename(initialdir="D:/", title="Upload an image")
+    filename = os.path.basename(os.path.splitext(file)[0])
     my_image = Image.open(file).convert("RGBA")
     txt = Image.new("RGBA", my_image.size, (255, 255, 255, 0))
     width, height = my_image.size
@@ -20,6 +22,7 @@ def add_watermark():
     d.text((x, y), user_input, font=font, fill=(255, 255, 255, 128), anchor="ms")
     image_output = Image.alpha_composite(my_image, txt)
     image_output.show()
+    image_output.save(f"{filename}_modified.png")
 
 
 root = Tk()
@@ -30,7 +33,6 @@ watermark_label = Label(root, text="Enter Text for Watermark", bg="black", fg="g
 watermark_label.pack(pady=10)
 text_entry = Entry(root, bg="gray", fg="white", width=30)
 text_entry.pack(pady=10)
-button = Button(root, text="Upload your Image", bg="blue", padx=10, pady=5, fg="white", command=add_watermark)
-button.pack(pady=10)
-
+watermark_button = Button(root, text="Upload your Image", bg="blue", padx=10, pady=5, fg="white", command=add_watermark)
+watermark_button.pack(pady=10)
 root.mainloop()
